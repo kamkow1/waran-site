4629cee9a6d99f520b5ddf346901a7489af21acf
kamkow1 <ikamileq@gmail.com>
2022-04-26 15:14:07 -0700

    added section component

--- pages/docs.js
+++ pages/docs.js
@@ -1,8 +1,16 @@
+import SectionHeader from '../components/Docs/Headers/SectionHeader'
+import Section from '../components/Docs/Section';
+import styles from '../styles/Docs.module.scss'
+
 export default function Docs() {
     return (
         <div>
-            <div className='p-5 text-center text-light'>
-                aqq
+            <div className={`p-5 text-center ${styles.content}`}>
+                <SectionHeader text='installation' />
+
+                <Section>
+                    
+                </Section>
             </div>
         </div>
     );
