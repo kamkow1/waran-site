1b988cabb8a2f4fd728df79d1ff592a766faa0e1
kamkow1 <ikamileq@gmail.com>
2022-04-26 15:38:45 -0700

    docs: installation section

--- pages/docs.js
+++ pages/docs.js
@@ -1,6 +1,7 @@
 import SectionHeader from '../components/Docs/Headers/SectionHeader'
 import Section from '../components/Docs/Section';
 import styles from '../styles/Docs.module.scss'
+import { CodeBlock, dracula } from 'react-code-blocks';
 
 export default function Docs() {
     return (
@@ -9,7 +10,15 @@ export default function Docs() {
                 <SectionHeader text='installation' />
 
                 <Section>
-                    
+                    to install the waran compiler, run
+                    with npm
+                    <div className={styles.code_bl}>
+                        <CodeBlock text='npm i -g waran' theme={dracula} language='js' showLineNumbers={false} />
+                    </div>
+                    or with yarn
+                    <div className={styles.code_bl}>
+                        <CodeBlock text='yarn global add waran' theme={dracula} language='js' showLineNumbers={false} />
+                    </div>
                 </Section>
             </div>
         </div>
